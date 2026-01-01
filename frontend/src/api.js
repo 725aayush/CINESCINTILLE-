3af2b77b723220a5bdbeb39bae927202c170a164
Aayush Pareek <153154221+725aayush@users.noreply.github.com>
2026-01-01 20:06:09 +0530

    Add comment
    Added comment to clarify the necessity of withCredentials.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://cinescintille-backend.onrender.com",
-  withCredentials: true
+  withCredentials: true,   //  REQUIRED
 });
 
 export default api;
